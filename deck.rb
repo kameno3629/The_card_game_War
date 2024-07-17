fe11c143056d1615926d2280fbad54130898f873
kameno3629 <blueocean777k@gmail.com>
2024-07-17 17:23:08 +0900

    [Add]Deck_class_skeleton

--- deck.rb
+++ deck.rb
@@ -1,2 +1,9 @@
 # debugツールの定義
 require 'debug'
+
+class Deck
+    # attr_readerでcardsを引っ張れるようにする
+    # 52枚のカードを生成（@cardsインスタンス変数）
+    # @cardsの順番をランダムにシャッフルします。
+    # プレイヤーの人数に応じてカードを均等に分割し、配布します。
+end
