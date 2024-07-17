523705725995950fcf5c1b054bfb943b578658b9
kameno3629 <blueocean777k@gmail.com>
2024-07-17 17:13:24 +0900

    [Add]Card_class_skeleton

--- card.rb
+++ card.rb
@@ -1,2 +1,13 @@
 # debugツールの定義
 require 'debug'
+
+# カードの要素を定義するクラス
+class Card
+    # attr_readerで値を引っ張れるようにする
+    # 配列スート、ランク、バリューをそれぞれ設定する。
+    # initializedで初期化
+    # to_sでカードの情報を文字列で表示できるようにする
+end
+
+
+
