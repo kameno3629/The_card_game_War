27c8784356bac1b0518ce9bd9c3789a97054e687
kameno3629 <blueocean777k@gmail.com>
2024-07-18 16:22:51 +0900

    [Rewrite]Deck_class_method_details

--- deck.rb
+++ deck.rb
@@ -3,25 +3,21 @@ require 'debug'
 
 # デッキの定義をするクラス
 class Deck
-    # attr_readerで他クラスからcardsを引っ張れるようにする
-    attr_reader :cards
-    # 52枚のカードを生成(カードをcardクラスから作成するだけで、デッキはまだ作成していません)（@cardsインスタンス変数）
+    attr_reader :cards # attr_readerで値を引っ張れるようにする
+  
     def initialize
-        @cards = [] #空白を入れて初期化
-        Card::SUIT.each do |suit| # suitはこのブロック内だけで有効なブロック変数です。SUITはCardクラスで定義した定数です。
-        # Card::SUITは、Cardクラスに属するSUIT定数という意味です。
-            Card::RANK.each do |rank| #rankはこのブロック内だけで有効なブロック変数です。RANKはCardクラスで定義した定数です。
-                @cards << Card.new(suit, rank, Card::VALUE[rank]) # ()内の変数はVALUE以外すべてブロック変数です。VALUEはCardクラスで定義した定数です。
-            end
-        end
+      suits = Card.cards # スートを取得
+      ranks = Card.cards # ランクを取得
+       # スートとランクを組み合わせてカードを作成
+      @cards = suits.product(ranks).map { |suit, rank| Card.new(suit, rank) }
+      shuffle # カードをシャッフルする
     end
-    # @cardsの順番をランダムにシャッフルします。
-    def shuffle
-        @cards.shuffle
+  
+    def shuffle # 再度シャッフルしたいときに呼び出す
+      @cards.shuffle!
     end
-    # プレイヤーの人数に応じてカードを均等に分割し、配布します。
-    #（ここで実際に配布はしていません。後にusers.rb内でDeck.newし、生成されたDockインスタンスを通じてdealメソッドが使われます）
-    def deal
-        @cards.shift # デッキの先頭から1枚のカードを取り出し、そのカードを返します。
+  
+    def deal(num_players)
+      @cards.each_slice(num_players).to_a # カードを配布したいときに呼び出す
     end
 end
