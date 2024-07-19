398e8b6a905de07405a7a2e30a1cbfb71607fe2d
kameno3629 <blueocean777k@gmail.com>
2024-07-19 14:12:04 +0900

    [Add]Users_class_method_details

--- users.rb
+++ users.rb
@@ -2,9 +2,18 @@
 require 'debug'
 
 class Users
-# attr_readerを設定し、手札とプレイヤーネームを引っ張れるようにする
-# initializeメソッドでプレイヤーネームと手札を初期化
-# 配られたカードを手札に追加
-# 手札の一番上のカードを取り出して返す
-# 手札が空かどうかの確認
+    # attr_readerを設定し、手札とプレイヤーネームを引っ張れるようにする
+    attr_reader :hand, :name
+    # initializeメソッドでプレイヤーネームと手札を初期化
+    def initialized(name)
+        @name = name # プレイヤーネームを設定
+        @hand = [] # 手札を空の配列で初期化
+    end
+        
+    def receive_cards(cards) # 配られたカードを手札に追加するメソッド
+        @hand.concat(cards)
+    end
+    def play_card # 手札から一番上のカードを取り出して返すメソッド
+        @hand.shift
+    end
 end
