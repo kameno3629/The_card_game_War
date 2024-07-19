e915fe2b32ac7baf7820340c1feab9274bcc7716
kameno3629 <blueocean777k@gmail.com>
2024-07-19 16:04:53 +0900

    [Edit]Change_Player_to_Users

--- gameprogress.rb
+++ gameprogress.rb
@@ -4,14 +4,14 @@ require 'debug'
 # 各ファイルの読み込み
 require_relative 'card'
 require_relative 'deck'
-require_relative 'player'
+require_relative 'users'
 require_relative 'game'
 
 # プレイヤーの名前の設定
-player_names = ['player1', 'player2']
+user_names = ['User1', 'User2']
 
 # ゲームを初期化
-game = Game.new(player_names)
+game = Game.new(user_names)
 
 # ゲームを開始
 game.play_game
