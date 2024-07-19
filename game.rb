c5636365417511047cef107dca93621a4027b859
kameno3629 <blueocean777k@gmail.com>
2024-07-19 16:09:46 +0900

    [Edit]Change_Player_to_Users

--- game.rb
+++ game.rb
@@ -2,27 +2,27 @@
 require 'debug'
 
 class Game
-    def initialize(player_names) # プレイヤーの名前リストを受け取り、プレイヤーオブジェクトを作成
-      @player_names = player_names.map { |name| User.new(name) }
+    def initialize(user_names) # プレイヤーの名前リストを受け取り、プレイヤーオブジェクトを作成
+      @user_names = user_names.map { |name| User.new(name) }
       @deck = Deck.new # デッキを生成
       deal_cards # カードを配ります
     end
     
     def deal_cards # プレイヤーにカードを配るメソッド
-      hands = @deck.deal(@players.size) # デッキからカードを配る
-      @players.each_with_index do |player, index| # プレイヤーにカードを配る
-        player.receive_cards(hands[index])
+      hands = @deck.deal(@users.size) # デッキからカードを配る
+      @users.each_with_index do |user, index| # プレイヤーにカードを配る
+        user.receive_cards(hands[index])
     end
     
     def play_round # ラウンドを行うメソッド
-        played_cards = @players.map { |player| [player, player.play_card] } # プレイヤーがカードを出す
+        played_cards = @users.map { |user| [user, user.play_card] } # プレイヤーがカードを出す
         winner = played_cards.max_by { |_, card| card.value }[0] # カードの値で勝者を決める
         winner.receive_cards(played_cards.map { |_, card| card }) # 勝者にカードを配る
         puts "#{winner.name} wins this round!" # 勝者の名前を表示
     end
 
     def play_game # ゲームを行うメソッド
-        until @players.any? { |player| player.hand.empty? } # プレイヤーの手札がなくなるまでラウンドを繰り返す
+        until @users.any? { |user| user.hand.empty? } # プレイヤーの手札がなくなるまでラウンドを繰り返す
           play_round
         end
         puts "Game over!" # ゲーム終了のメッセージを表示
