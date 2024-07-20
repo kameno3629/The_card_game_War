369825df4611e3dc395429ad828140f0069b8973
kameno3629 <blueocean777k@gmail.com>
2024-07-21 07:30:58 +0900

    [Add]Draw&SubHand_Feature

--- game.rb
+++ game.rb
@@ -5,52 +5,43 @@ require_relative 'users'
 require_relative 'deck'
 
 class Game
-    def initialize(user_names) # プレイヤーの名前リストを受け取り、プレイヤーオブジェクトを作成
-      @user_names = user_names.map { |name| Users.new(name) }
-      @deck = Deck.new # デッキを生成
-      deal_cards # カードを配ります
-    end
-    
-    def deal_cards # プレイヤーにカードを配るメソッド
-      hands = @deck.deal(@users.size) # デッキからカードを配る
-      @users.each_with_index do |user, index| # プレイヤーにカードを配る
-        user.receive_cards(hands[index])
-      end
-    end
-    
-    def play_round # ラウンドを行うメソッド
-        played_cards = @users.map { |user| [user, user.play_card] } # プレイヤーがカードを出す
-        winner = played_cards.max_by { |_, card| card.value }[0] # カードの値で勝者を決める
-        winner.receive_cards(played_cards.map { |_, card| card }) # 勝者にカードを配る
-        puts "#{winner.name} wins this round!" # 勝者の名前を表示
-    end
+  def initialize(user_names) # プレイヤーの名前リストを受け取り、プレイヤーオブジェクトを作成
+    @users = user_names.map { |name| Users.new(name) } # 各プレイヤーのオブジェクトを作成
+    @deck = Deck.new # デッキを生成
+    deal_cards # カードを配ります
+  end
 
-    def play_game # ゲームを行うメソッド
-        until @users.any? { |user| user.hand.empty? } # プレイヤーの手札がなくなるまでラウンドを繰り返す
-          play_round
-        end
-        puts "Game over!" # ゲーム終了のメッセージを表示
+  def deal_cards # プレイヤーにカードを配るメソッド
+    hands = @deck.deal(@users.size) # デッキからプレイヤーの数に応じてカードを配る
+    @users.each_with_index do |user, index| # 各プレイヤーにカードを配る
+      user.receive_cards(hands[index]) # プレイヤーにカードを渡す
     end
+  end
 
+  def play_round(played_cards = []) # ラウンドを行うメソッド
+    new_played_cards = @users.map { |user| [user, user.play_card] } # 各プレイヤーがカードを1枚出す
+    played_cards.concat(new_played_cards) # 既に場に出ているカードに追加する
+
+    # 勝者を決定する
+    max_value = new_played_cards.map { |_, card| card.value }.max # 出されたカードの中で最大の値を取得
+    winners = new_played_cards.select { |_, card| card.value == max_value }.map { |user, _| user } # 最大の値を持つカードを出したプレイヤーを取得
+
+    if winners.size > 1 # 引き分けの場合
+      puts "引き分けです。もう一枚カードを出します。" # 引き分けメッセージを表示
+      play_round(played_cards) # 引き分けの場合、再度カードを出す
+    else
+      winner = winners.first # 勝者を決定
+      winner_cards = played_cards.map { |_, card| card } # 勝者のカードを配列で取得
+      winner.receive_cards(winner_cards) # 勝者にカードを配る
+      puts "#{winner.name}が勝ちました。" # 勝者の名前を表示
+    end
+  end
+
+  def play_game # ゲームを行うメソッド
+    puts "戦争を開始します。" # ゲーム開始のメッセージを表示
+    until @users.any? { |user| user.total_cards == 0 } # プレイヤーの手札がなくなるまでラウンドを繰り返す
+      play_round # ラウンドを実行
+    end
+    puts "戦争を終了します" # ゲーム終了のメッセージを表示
+  end
 end
-
-    
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
