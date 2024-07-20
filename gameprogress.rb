d2fb42dedac5e291d1f677ecac02b8f83ef8c97f
kameno3629 <blueocean777k@gmail.com>
2024-07-21 07:36:31 +0900

    [Edit]Number_of_Players

--- gameprogress.rb
+++ gameprogress.rb
@@ -8,7 +8,7 @@ require_relative 'users'
 require_relative 'game'
 
 # プレイヤーの名前の設定
-user_names = ['User1', 'User2']
+user_names = ['User1', 'User2', 'User3', 'User4']
 
 # ゲームを初期化
 game = Game.new(user_names)
